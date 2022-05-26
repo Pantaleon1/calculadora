688725cc1e2233ec5fcb306bb9bb3dc1aedcdb5f
Carlos Manuel Pantaleon Miguel <pantaleoncarlos937@gmail.com>
2022-05-26 00:16:17 -0500

    historial

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ function App() {
   const [currentOperation, setCurrentOperaton] = useState("");
   const [result, setResult] = useState(0);
 
+  const init= JSON.parse(localStorage.getItem("historial_cal"))||[];
+  const [historial, setHistorial]= useState(init);
+
   
 
   function allClear(){
+
+    const lista = {num1:number1, num2:number2, ope:currentOperation, res:result}
+    const nuevoarreglo=[...historial, lista]
+    setHistorial([...nuevoarreglo]);
+    localStorage.setItem("historial_cal", JSON.stringify(nuevoarreglo));
+    
+
+
     setNumber1("");
     setNumber2("");
     setCurrentOperaton("");
@@ -50,7 +61,7 @@ function App() {
     if (currentOperation === " ") {
       setNumber1(number1.toString().slice(0, -1));
     } else {
-      setNumber2(number2.toString().slice(0, -1));
+      setNumber2(number2.toString().slice(0, -1)); 
     }
   };
   return (
@@ -62,7 +73,7 @@ function App() {
            <div className='current-operand'>{result ? result : (!currentOperation ? number1 : number2) }</div>
        </div>
        <button onClick={allClear} className="span-two" >AC</button>
-       <button onClick={() => {}}>DEL</button>
+       <button onClick={() => {deleteNumber()}}>DEL</button>
        <button onClick={() => {clickOperation("/")}}>/</button>
        <button onClick={() => {clickNumber(7)}}>7</button>
        <button onClick={() => {clickNumber(8)}}>8</button>
@@ -80,6 +91,31 @@ function App() {
        <button onClick={() => {clickNumber(0)}}>0</button>
        <button onClick={getResult}className="span-two">=</button>
      </div>
+   
+    
+    <div className= "col" style={{width: 400}}>
+      <span className="row">
+        <div className='col'
+        style={{width: 400, left: 0}}
+        >
+          <h3 >Historial de operaciones</h3>
+          {historial.leng === 0 ? ("vacio")
+          : (
+            <ol>
+              {historial.map((item, index) =>{
+                return (
+                  <li key={index}>
+                    {item.num1}{item.ope}{item.num2}= {item.res} &nbsp;
+                  </li>
+                );
+              })}
+            </ol>
+          )}
+
+        
+        </div>
+      </span>
+    </div>
     </div>
   );
 }
